87c51f1bb2c44a5ebdf21ea5041ac8fb958b2446
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-04 22:22:20 +0300

    решена задача с заменой знаков в массиве

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.Clear();
+Console.WriteLine("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] array = new int [size];
+int [] array2 = new int [size];
+
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-99,100);
+        Console.Write(array[i]+" ");
+    }
+    
+    Console.WriteLine();
+    
+    for(int i = 0; i < size; i++)
+    {
+        array2[i] = array[i]*-1;
+        Console.Write(array2[i]+" ");
+    }
+
+
